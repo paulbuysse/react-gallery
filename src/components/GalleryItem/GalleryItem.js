0eaf75a8a9be813b0aead9139da6b32086750ba8
Paul Buysse <paulbuysse001@gmail.com>
2020-08-14 18:58:24 -0500

    got the like phrases working

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -3,10 +3,22 @@ import './GalleryItem.css';
 
 class GalleryItem extends Component {
     render() {
+        const numOfLikes = this.props.pic.likes
+        let phraseForLikes = ''
+
+        if (numOfLikes === 0) {
+            phraseForLikes = `Nobody likes this! :(`
+        } else if (numOfLikes === 1) {
+            phraseForLikes = `${numOfLikes} person likes this!`
+        } else {
+            phraseForLikes = `${numOfLikes} people like this!`
+        }
+
         return (
             <div>
-                <p>dkjn</p>
                 <img src={this.props.pic.path} />
+                <h3>{phraseForLikes}</h3>
+                <button>like it!</button>
             </div>
         )
     }
